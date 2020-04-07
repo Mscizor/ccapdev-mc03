c7074176fe681892b089814fb8fec078d586bfeb
Jan Uriel Marcelo <janurielm@gmail.com>
2020-04-07 13:47:32 +0800

    Add User schema to models

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,13 +1,15 @@
 
 var mongoose = require('mongoose');
 
-/*
-    TODO:   Complete the UserSchema which will contain the name and the
-            number of contacts in the database.
-*/
-
 var UserSchema = new mongoose.Schema({
-    // your code here
+  name: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: Number,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('User', UserSchema);
